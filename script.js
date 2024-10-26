f40ca7da67d2072a9bab6ae1a9e4fd7bd5ed6a29
Samson Mesioye <jazzygee36gmail.com>
2024-10-26 11:15:32 +0100

    bottom border top

--- script.js
+++ script.js
@@ -250,3 +250,14 @@ function toggleAccordion(element) {
     content.style.display = 'block';
   }
 }
+document.querySelectorAll('.bottom-menu li').forEach((item) => {
+  item.addEventListener('click', function () {
+    // Remove 'active' class from all items
+    document
+      .querySelectorAll('.bottom-menu li')
+      .forEach((el) => el.classList.remove('active'));
+
+    // Add 'active' class to the clicked item
+    this.classList.add('active');
+  });
+});
